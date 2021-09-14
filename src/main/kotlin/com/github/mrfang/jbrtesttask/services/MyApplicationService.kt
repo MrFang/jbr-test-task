b6486dab9c82e903e8b77890d5836a32969e435e
GitHub Action <action@github.com>
2021-09-14 06:02:58 +0000

    Template cleanup

--- src/main/kotlin/com/github/mrfang/jbrtesttask/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/mrfang/jbrtesttask/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.mrfang.jbrtesttask.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mrfang.jbrtesttask.MyBundle
 
 class MyApplicationService {
 
--- src/test/kotlin/com/github/mrfang/jbrtesttask/MyPluginTest.kt
+++ src/test/kotlin/com/github/mrfang/jbrtesttask/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.mrfang.jbrtesttask
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.psi.xml.XmlFile
